10c61a0a5b2033bff0cbeeea60fdd910596e8cb5
Chinmay-innovates <chinmaygope69@gmail.com>
2024-01-19 19:53:42 +0530

    add sorting mehords

--- DS_Project/HeapSort.c
+++ DS_Project/HeapSort.c
@@ -2,19 +2,16 @@
 
 #include <stdio.h>
 
-// Function to swap the position of two elements
+// Function to swap two elements
 void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-// To heapify a subtree rooted with node i
-// which is an index in arr[].
-// n is size of heap
 void heapify(int arr[], int N, int i)
 {
- // Initialize largest as root
+    // Initialize largest as root
     int largest = i;
 
     int left = 2 * i + 1;
@@ -38,8 +35,6 @@ void heapify(int arr[], int N, int i)
 // Main function to do heap sort
 void heapSort(int arr[], int N)
 {
-
-    // Build max heap
     for (int i = N / 2 - 1; i >= 0; i--)
 
         heapify(arr, N, i);
@@ -47,12 +42,7 @@ void heapSort(int arr[], int N)
     // Heap sort
     for (int i = N - 1; i >= 0; i--)
     {
-
         swap(&arr[0], &arr[i]);
-
-        // Heapify root element
-        // to get highest element at
-        // root again
         heapify(arr, i, 0);
     }
 }
@@ -71,12 +61,21 @@ int main()
     int arr[] = {12, 55, 64, 11, 13, -5, 6, 7};
     int N = sizeof(arr) / sizeof(arr[0]);
     printf("UnSorted array is:\n");
-    for (int i = 0; i < N; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+    printArray(arr, N);
     // function call
     heapSort(arr, N);
     printf("\nSorted array is:\n");
     printArray(arr, N);
 }
+
+/*                                        OUTPUT
+PS C:\VS-code\.vscode\Data Structures\DS_Project> cd "c:\VS-code\Data Structures\DS_Project\"
+; if ($?) { gcc heapSort.c -o heapSort } ; if ($?) { .\heapSort }
+
+UnSorted array is:
+12 55 64 11 13 -5 6 7
+
+Sorted array is:
+-5 6 7 11 12 13 55 64
+PS C:\VS-code\.vscode\Data Structures\DS_Project>
+*/
