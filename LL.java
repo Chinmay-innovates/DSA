40c995acf3d5139940554c310fd405da13cff280
Chinmay-innovates <chinmaygope69@gmail.com>
2025-01-11 18:05:41 +0530

    sorting and linked list

---
+++ LL.java
@@ -0,0 +1,195 @@
+
+class LL {
+    Node head;
+    private int size;
+
+    LL() {
+        this.size = 0;
+    }
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node currNode = head;
+
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public void removeFirst() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        size--;
+        head = head.next;
+        System.out.println("First element deleted");
+    }
+
+    public void removeLast() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        size--;
+
+        if (head.next == null) {
+            head = null;
+            System.out.println("List is now empty");
+            return;
+        }
+
+        Node currNode = head;
+
+        while (currNode.next.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = null;
+        System.out.println("Last element deleted");
+    }
+
+    public void remove(String data) {
+        if (head == null) {
+            System.out.println("Cannot delete, list is empty");
+            return;
+        }
+
+        Node currNode = head;
+
+        while (currNode.next != null) {
+            if (currNode.next.data.equals(data)) {
+                currNode.next = currNode.next.next;
+                size--;
+                System.out.println("Data '" + data + "' deleted");
+                return;
+            }
+            currNode = currNode.next;
+        }
+        System.out.println("Data '" + data + "' not found");
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Node reverseList(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public Node reverseIterative(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node prevNode = head;
+        Node currNode = head.next;
+
+        while (currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+
+        return prevNode;
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.addFirst("c");
+        list.print();
+        list.addFirst("b");
+        list.print();
+        list.addFirst("a");
+        list.print();
+
+        list.addLast("d");
+        list.print();
+        list.addLast("e");
+        list.print();
+        list.addLast("f");
+        list.print();
+        list.addLast("g");
+        list.print();
+        list.addLast("h");
+        list.print();
+
+        // list.remove("b");
+        // list.removeFirst();
+        // list.removeLast();
+
+        list.print();
+        list.head = list.reverseIterative(list.head);
+        list.print();
+
+        // System.out.println("Size: " + list.getSize());
+
+        // LinkedList<Integer> ll = new LinkedList<Integer>();
+        // ll.addFirst(5);
+        // ll.addLast(6);
+        // ll.addLast(7);
+        // ll.addLast(8);
+
+        // System.out.println(ll);
+        // System.out.println(ll.size());
+
+        // for (int s : ll) {
+        // System.out.print(s + " -> ");
+        // }
+        // System.out.println("NULL");
+
+    }
+}
---
+++ Sorting.java
@@ -0,0 +1,181 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sorting {
+
+    private static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+        printArray(arr);
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int low = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[low]) {
+                    low = j;
+                }
+            }
+            if (low != i) {
+                swap(arr, low, i);
+            }
+        }
+
+        printArray(arr);
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        printArray(arr);
+    }
+
+    private static void merge(int[] arr, int st, int mid, int end) {
+        List<Integer> temp = new ArrayList<Integer>();
+
+        int i = st, j = mid + 1;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] < arr[j]) {
+                temp.add(arr[i]);
+                i++;
+            } else {
+                temp.add(arr[j]);
+                j++;
+            }
+        }
+        while (i <= mid) {
+            temp.add(arr[i]);
+            i++;
+        }
+        while (j <= end) {
+            temp.add(arr[j]);
+            j++;
+        }
+
+        for (int idx = 0; idx < temp.size(); idx++) {
+            arr[st + idx] = temp.get(idx);
+        }
+
+    }
+
+    public static void mergeSort(int[] arr, int st, int end) {
+        if (st < end) {
+            int mid = st + (end - st) / 2;
+
+            mergeSort(arr, st, mid);
+            mergeSort(arr, mid + 1, end);
+
+            merge(arr, st, mid, end);
+        }
+    }
+
+    private static int partition(int[] arr, int st, int end) {
+        int pivot = arr[end];
+        int idx = st - 1;
+        for (int j = st; j < end; j++) {
+            if (arr[j] < pivot) {
+                idx++;
+                swap(arr, j, idx);
+            }
+        }
+        idx++;
+        swap(arr, idx, end);
+        return idx;
+    }
+
+    public static void quickSort(int[] arr, int st, int end) {
+        if (st < end) {
+            int pivIdx = partition(arr, st, end);
+            quickSort(arr, st, pivIdx - 1);
+            quickSort(arr, pivIdx + 1, end);
+        }
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        if (largest != i) {
+            swap(arr, largest, i);
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void heapSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+
+            // Move current root to end
+            swap(arr, 0, i);
+
+            heapify(arr, i, 0);
+        }
+        printArray(arr);
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 4, 2, -1 };
+        printArray(arr);
+
+        System.out.println("After sorting");
+        bubbleSort(arr);
+
+        selectionSort(arr);
+        
+        insertionSort(arr);
+
+        mergeSort(arr, 0, arr.length - 1);
+        printArray(arr);
+
+        quickSort(arr, 0, arr.length - 1);
+        printArray(arr);
+
+        heapSort(arr);
+
+    }
+}
